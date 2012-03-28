4ae698643c9301971afe33b419a468872163342f
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-03-28 17:34:50 -0400

    Add start of Riak CS provider in Fog.
    Few comments:
    
    I didn't use the Fog::Storage namespace, because I thought we might want
    to alias Fog::RiakCS::Storage to Fog::AWS::Storage, so people could
    freely type Fog::Storage.new(:provider => 'riak_cs') and have it "just
    work".  This left me to stuff the first feature, user creation, under a
    new area called Administration.  This is where I'll stick usage
    statistics gathering as well, along with delete/disable and list user.
    Maybe there is a better name for this, not sure.  I'm considering these
    features as a "superset" Rick CS provides of the AWS functionality.

---
+++ lib/fog/riak_cs/administration.rb
@@ -0,0 +1,102 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'riak_cs'))
+
+module Fog
+  module RiakCS
+    class Administration < Fog::Service
+
+      class UserAlreadyExists < Fog::RiakCS::Administration::Error; end
+
+      recognizes :host, :path, :port, :scheme, :persistent
+
+      request_path 'fog/riak_cs/requests/administration'
+      request :create_user 
+
+      module Utils
+        def configure_uri_options(options = {})
+          @host       = options[:host]       || 'localhost'
+          @persistent = options[:persistent] || true
+          @port       = options[:port]       || 8080
+          @scheme     = options[:scheme]     || 'http'
+        end
+
+        def riak_cs_uri
+          "#{@scheme}://#{@host}:#{@port}"
+        end
+      end
+
+      class Mock
+        include Utils
+
+        def self.data
+          @data ||= Hash.new do |hash, key| 
+            hash[key] = {
+              :users => {
+                "existing@example.com" => "Preexisting user"
+              }
+            }
+          end
+        end
+
+        def self.reset
+          @data = nil
+        end
+
+        def initialize(options = {}) 
+          configure_uri_options(options)
+        end
+
+        def data
+          self.class.data[riak_cs_uri]
+        end
+
+        def reset_data
+          self.class.data.delete(riak_cs_uri)
+        end
+      end
+
+      class Real
+        include Utils
+
+        def initialize(options = {})
+          require 'mime/types'
+          require 'multi_json'
+          require 'multi_xml'
+
+          configure_uri_options(options)
+          @connection_options = options[:connection_options] || {}
+          @persistent         = options[:persistent]         || false
+
+          @connection = Fog::Connection.new(riak_cs_uri, @persistent, @connection_options)
+        end
+
+        def request(params, parse_response = true, &block) 
+          begin
+            response = @connection.request(params.merge!({
+              :host     => @host,
+              :path     => "#{@path}/#{params[:path]}",
+            }), &block)
+          rescue Excon::Errors::HTTPStatusError => error
+            if match = error.message.match(/<Code>(.*)<\/Code>(?:.*<Message>(.*)<\/Message>)?/m)
+              case match[1]
+              when 'UserAlreadyExists'
+                raise Fog::RiakCS::Administration.const_get(match[1]).new
+              else
+                raise error
+              end
+            end
+          end
+          if !response.body.empty? && parse_response
+            case response.headers['Content-Type']
+            when 'application/json'
+              response.body = MultiJson.decode(response.body)
+            when 'application/xml'
+              response.body = MultiXml.parse(response.body)
+            end
+          end
+          response
+        end
+      end
+
+    end
+  end
+end
